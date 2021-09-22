12d99db9fd8c27dc61f3b4ea1fc9bf112e52729c
Ferguson Osas <ebenezerosas92@gmail.com>
2021-09-22 15:10:20 +0100

    correct for netlify

--- src/RightComponents/Login_form/Form.jsx
+++ src/RightComponents/Login_form/Form.jsx
@@ -4,22 +4,49 @@ import Button from "../../UI/Button/Button";
 import companyLogo from "../../icons/logo.svg";
 
 const Form = () => {
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+
+  const usernameHandler = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const passwordHandler = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+  };
+
+  console.log(username, password);
+
   return (
     <FormStyle>
       <img src={companyLogo} alt="logo" className="formBigLogo" />
-      <img src={companyLogo} alt="logo" className='logo'/>
+      <img src={companyLogo} alt="logo" className="logo" />
       <div className="wrapper">
         <div className="write-up">
           Login to your <br /> Dashboard
         </div>
-        <form>
+        <form onSubmit={onSubmitHandler}>
           <div>
-            <input type="text" placeholder="username" required />
+            <input
+              type="email"
+              placeholder="email"
+              required
+              onChange={usernameHandler}
+            />
           </div>
           <div>
-            <input type="text" placeholder="password" required />
+            <input
+              type="password"
+              placeholder="password"
+              required
+              onChange={passwordHandler}
+            />
           </div>
-          <Button>LOGIN</Button>
+          <Button type="submit">LOGIN</Button>
         </form>
       </div>
     </FormStyle>
